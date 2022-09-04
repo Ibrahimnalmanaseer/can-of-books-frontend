58f6efb7ba3afffdd4cb57cca81c29cc704fe361
Ibrahimnalmanaseer <ibrahimnalmanaseer@gmail.com>
2022-09-04 21:02:41 +0300

    feature

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import axios from 'axios';
+import img from './images/bokke.jpg'
+
+import Carousel from 'react-bootstrap/Carousel';
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -8,7 +12,17 @@ class BestBooks extends React.Component {
     }
   }
 
-  /* TODO: Make a GET request to your API to fetch all the books from the database  */
+  componentDidMount=async()=>{
+
+    await axios.get('http://localhost:3001/books')
+    .then(result=>{
+
+      this.setState({
+        books:result.data
+      })
+      
+    })
+  }
 
   render() {
 
@@ -17,12 +31,34 @@ class BestBooks extends React.Component {
     return (
       <>
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
+                  
+                    {this.state.books.length ?  
+                    
+                    
+                    <Carousel>
+                      {this.state.books.map(item => (
+                      <Carousel.Item>
+                        <img
+                          className="d-block w-100"
+                          src={img}
+                          alt={item.title}
+                          style={{height: 400 , width:100}}
+                        />
+                        <Carousel.Caption>
+                          <h3>{item.title}</h3>
+                          <p>{item.description}</p>
+                        </Carousel.Caption>
+                      </Carousel.Item>
+                     ) )}
 
-        {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
-        ) : (
+
+            </Carousel>
+                 
+        : (
           <h3>No Books Found :(</h3>
         )}
+
+
       </>
     )
   }
