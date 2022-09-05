17417be4d97e740e3443c377baba49aedc829980
Ibrahimnalmanaseer <ibrahimnalmanaseer@gmail.com>
2022-09-05 22:21:56 +0300

    feature

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import axios from 'axios';
-import img from './images/bokke.jpg'
-
+import img from './images/bokke.jpg';
+import ModalBook  from './modal';
+import Button from 'react-bootstrap/Button';
 import Carousel from 'react-bootstrap/Carousel';
 
 class BestBooks extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+
+      show:false,
       books: []
     }
   }
+
+
 
   componentDidMount=async()=>{
 
@@ -21,8 +26,52 @@ class BestBooks extends React.Component {
         books:result.data
       })
       
+      
     })
   }
+  
+ handeleclose=()=>{
+
+  this.setState({
+    show:false
+  })
+
+ } 
+
+
+ handlebutton=()=>{
+
+  this.setState({show:true})
+  
+ } 
+
+ updateBooks=(data)=>{
+
+  this.setState({
+    books:data
+
+  })
+
+ }
+
+ 
+
+ deleteHandle=(id)=>{
+  
+  
+  axios
+  .delete(`http://localhost:3001/deletebook/${id}`) 
+  .then(result =>{
+    this.setState({
+      books:result.data
+    })
+    
+
+ })}
+
+ 
+
+
 
   render() {
 
@@ -30,7 +79,17 @@ class BestBooks extends React.Component {
 
     return (
       <>
+        <ModalBook newData={this.updateBooks} show={this.state.show}  closeShow={this.handeleclose} />
+
+        
+
+       
+
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
+
+        <Button variant="secondary"  onClick={this.handlebutton}>ADD YOUR FAV BOOK! </Button>
+
+          
                   
                     {this.state.books.length ?  
                     
@@ -38,17 +97,26 @@ class BestBooks extends React.Component {
                     <Carousel>
                       {this.state.books.map(item => (
                       <Carousel.Item>
+
+                      
                         <img
                           className="d-block w-100"
                           src={img}
                           alt={item.title}
                           style={{height: 400 , width:100}}
                         />
+                        
                         <Carousel.Caption>
+                        
                           <h3>{item.title}</h3>
                           <p>{item.description}</p>
+                        
+                          <Button variant="danger"  onClick={()=>this.deleteHandle(item._id)}>Delete Book </Button>
+                          
                         </Carousel.Caption>
                       </Carousel.Item>
+
+                      
                      ) )}
 
 
---
+++ src/modal.js
@@ -0,0 +1,124 @@
+
+
+// import Button from 'react-bootstrap/Button';
+// import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
+import React from "react";
+import axios from 'axios';
+
+
+class ModalBook extends React.Component{
+
+  
+
+
+
+  
+handleData=(event)=>{
+
+    event.preventDefault();
+    const obj={
+
+        title:event.target.title.value,
+        description:event.target.bookdisc.value,
+        status:event.target.status.value,
+    }
+
+    console.log(obj)
+    axios
+    .post('http://localhost:3001/addbook',obj)
+    .then(result=>{
+
+        
+        this.props.newData(result.data)
+
+    })
+
+    
+    
+    
+}
+
+
+
+    render(){
+
+
+
+        return (
+
+        <>
+        <Modal show={this.props.show} onHide={this.props.closeShow}>
+        <Modal.Header closeButton>
+          <Modal.Title> Book Details  </Modal.Title>
+        </Modal.Header>
+        <Modal.Body> 
+        <form onSubmit={this.handleData}>
+          <input type="text" name="title" placeholder='book name' />
+          <br></br>
+          <br></br>
+          <input type="text" name="bookdisc" placeholder='book dic' />
+          <br></br>
+          <br></br>
+          <input type="text" name="status" placeholder='bookstatus' />
+          <br></br>
+          <br></br>
+          <button type='submit'>Add</button>
+
+        </form>
+
+
+
+        {/* <Form>
+      <Form.Group className="mb-3" controlId="formBasictext" >
+        <Form.Label>Book Title</Form.Label>
+        <Form.Control type="text" placeholder="Enter Book Name" name='title' />
+        <Form.Text className="text-muted"  >
+          Book Title
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasictext">
+        <Form.Label>Discription </Form.Label>
+        <Form.Control type="text" placeholder="Enter Book Discription" name='bookdisc'/>
+        <Form.Text className="text-muted"  >
+        Discription
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasictext">
+        <Form.Label >Status</Form.Label>
+        <input type="text" name="status" placeholder='status ccc' />
+        <Form.Text className="text-muted"  >
+          Status
+        </Form.Text>
+      </Form.Group>
+
+      
+
+      
+      <Button variant="primary" type="submit"  onClick={this.handleData}>
+        Submit
+      </Button>
+    </Form>
+             */}
+            
+            
+            
+              </Modal.Body>
+        
+      </Modal>
+    </>
+        )
+    }
+
+
+
+
+
+
+
+
+}
+
+export default ModalBook ;
